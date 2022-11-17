e650709de75970227e8faea532c9f3146a005590
Rafal Michaluszek <rmichaluszek@gmail.com>
2022-11-18 00:19:27 +0100

    Complete third puzzle

--- 3/main.cpp
+++ 3/main.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 #include <iostream>
-#include <sstream>
-#include <algorithm>
 
 using namespace std;
 
@@ -33,11 +31,46 @@ int main()
 {
     vector<string> data = getData();
 
+    int bitsLength = data[0].length();
+
+    int numberOfOnes[bitsLength] = { 0 };
+    int numberOfZeros[bitsLength] = { 0 };
+    // later on, if the numberOfOnes[i] is greater than zeros, then the gamma rate for 'i' bit is 1, and epsilon is the opposite (0)
 
     for (size_t i = 0; i != data.size(); ++i) {
 
+        string dataLine = data[i];
+        for (int j = 0; j < bitsLength; j++) {
+            char c = dataLine[j];
+
+            if(c == '1') {
+                numberOfOnes[j]++;
+            } else {
+                numberOfZeros[j]++;
+            }
+        }
+        // count the amount of ones and zeros in each column (j) iterating trough every row (i)
     }
 
+    string gammaBits = "";
+    string epsilonBits = "";
+    // check which amount is greater in each of the columns (j)
+    for (int j = 0; j < bitsLength; j++) {
+        if (numberOfOnes[j] > numberOfZeros[j]) {
+            gammaBits += "1";
+            epsilonBits += "0";
+        } else {
+            gammaBits += "0";
+            epsilonBits += "1";
+        }
+    }
+
+    // convert from binary code into decimal (int)
+    int gammaPower = stoi(gammaBits, 0, 2);
+    int epsilonPower = stoi(epsilonBits, 0, 2);
+
+    // and print the result of the puzzle
+    cout << gammaPower*epsilonPower << endl;
     cin;
     // waits for any key to close the program, just to see results
 
