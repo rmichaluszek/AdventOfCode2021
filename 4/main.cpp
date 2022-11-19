c6b668af65af71022b184ca11ccc535f5bef98e5
Rafal Michaluszek <rmichaluszek@gmail.com>
2022-11-19 05:02:51 +0100

    Blank puzzle template

---
+++ 4/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+vector<string> getData() {
+// using container because of easier resizing, and i want to allow dynamic number of lines allowed
+
+    vector<string> data;
+
+    bool inputEnabled = true;
+
+    while(inputEnabled) {
+
+        string l = "";
+
+        getline(cin,l);
+
+        if(l.length()<= 0)
+            inputEnabled = false;
+            // if the input is empty, then stop the user input
+        else
+            data.push_back(l);
+            // if it's not empty, add the value to the container
+    }
+
+    return data;
+}
+
+int main()
+{
+    vector<string> data = getData();
+
+    for (size_t i = 0; i != data.size(); ++i) {
+
+    }
+    cin;
+    // waits for any key to close the program, just to see results
+
+    return 0;
+}
