74af4289624f26b4807b994ff78dfb547d7dcf35
Rafal Michaluszek <rmichaluszek@gmail.com>
2022-11-19 05:25:16 +0100

    Add splitting values into vector

--- 4/main.cpp
+++ 4/main.cpp
@@ -10,6 +10,9 @@ vector<string> getData() {
 
     bool inputEnabled = true;
 
+    // this time the data can have empty line, so input ends when empty line is entered 2 times in a row
+    int entersPressed = 0;
+
     while(inputEnabled) {
 
         string l = "";
@@ -17,21 +20,74 @@ vector<string> getData() {
         getline(cin,l);
 
         if(l.length()<= 0)
-            inputEnabled = false;
-            // if the input is empty, then stop the user input
-        else
+            entersPressed++;
+        else {
+           entersPressed = 0;
+        }
+        // if the input is empty, then stop the user input
+        if (entersPressed==0)
             data.push_back(l);
+        else if (entersPressed == 1)
+            data.push_back("\n");
+        else inputEnabled = false;
             // if it's not empty, add the value to the container
     }
 
     return data;
 }
 
+
+class BingoBoard {
+  public:
+
+    // 2d arrays with numbers of the board
+    int values[5][5] = {
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+    };
+
+    // 2d array of marked values (true/false)
+    bool markedValues[5][5] = {
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+    };
+
+    bool CheckForBingo() {
+        //
+        return false;
+    }
+};
+
 int main()
 {
     vector<string> data = getData();
 
+    vector<int> bingoValues;
+
     for (size_t i = 0; i != data.size(); ++i) {
+        // first line are values, and later on are bingo boards every "/n" line
+
+        if (i == 0) {
+
+            stringstream valuesStream(data[0]);
+
+            while(valuesStream.good()) {
+
+                // split values by comma
+                string str;
+                getline(valuesStream,str,',');
+                int value = stoi(str);
+                bingoValues.push_back(value);
+
+                cout << value << endl;
+            }
+        }
 
     }
     cin;
